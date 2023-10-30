50597ede76687e4cfb9b7a329424ed0d40e85c36
Aki Wu <wuminzhe@gmail.com>
2023-10-30 09:16:41 +0800

    message index page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,70 +1,32 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: %i[show edit update destroy]
+  before_action :set_message, only: %i[show]
 
   # GET /messages or /messages.json
   def index
-    @messages = Message.order(block_timestamp: :desc).limit(50)
+    @messages = Message.order(block_timestamp: :desc)
   end
 
   # GET /messages/1 or /messages/1.json
   def show; end
 
-  # GET /messages/new
-  def new
-    @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit; end
-
-  # POST /messages or /messages.json
-  def create
-    @message = Message.new(message_params)
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to message_url(@message), notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1 or /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to message_url(@message), notice: 'Message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /messages/1 or /messages/1.json
-  def destroy
-    @message.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: 'Message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_message
-    @message = Message.find(params[:id])
-  end
+    @message =
+      if params[:id].present?
+        Message.find(params[:id])
+      else
+        network = Pug::Network.find_by(name: params[:network])
+        Message.find_by(from_network: network, index: params[:index])
+      end
 
-  # Only allow a list of trusted parameters through.
-  def message_params
-    params.require(:message).permit(:index, :msg_hash, :root, :channel, :from_chain_id, :from, :to_chain_id, :to,
-                                    :block_number, :block_timestamp, :transaction_hash, :status, :encoded, :from_network_id, :to_network_id, :dispatch_transaction_hash, :dispatch_block_number, :dispatch_block_timestamp, :clear_transaction_hash, :clear_block_number, :clear_block_timestamp)
+    return unless @message.nil?
+
+    # 404
+    respond_to do |format|
+      format.html { render file: "#{Rails.root}/public/404.html", status: :not_found }
+      format.json { render json: { error: 'Not Found' }, status: :not_found }
+    end
   end
 end
