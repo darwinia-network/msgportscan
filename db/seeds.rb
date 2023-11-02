ab4a216c19f22be90f372665832884583c6a8014
Aki Wu <wuminzhe@gmail.com>
2023-11-02 11:15:13 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,34 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 puts '== Start seeding'
-puts `rails 'pug:add_contract[421614,0x0034607daf9c1dc6628f6e09e81bb232b6603a89]'`
-puts `rails 'pug:add_contract[421614,0x007eed6207c9af3715964fb7f8b5f44e002a3498]'`
-puts `rails 'pug:add_contract[421614,0x0002396f1d52323fcd1ae8079b38808f046882c3]'`
-puts `rails 'pug:add_contract[421614,0x00945C032A37454333d7044a52a5A42Aa0f6c608]'`
-puts `rails 'pug:add_contract[44,0x0034607daf9c1dc6628f6e09e81bb232b6603a89]'`
-puts `rails 'pug:add_contract[44,0x007eed6207c9af3715964fb7f8b5f44e002a3498]'`
-puts `rails 'pug:add_contract[44,0x0002396f1d52323fcd1ae8079b38808f046882c3]'`
-puts `rails 'pug:add_contract[44,0x00945C032A37454333d7044a52a5A42Aa0f6c608]'`
+
+puts '=== Import Pug::Network records ==='
+require 'open-uri'
+require 'json'
+
+networks = JSON.parse(URI.open('https://chainid.network/chains.json').read)
+networks.each do |network|
+  rpc_url = network['rpc']&.select { |url| url.start_with?('http') && url !~ /\$\{(.+)\}/ }&.first
+  explorer_url = network['explorers']&.first&.[]('url')
+  Pug::Network.create(
+    chain_id: network['chainId'],
+    name: network['shortName'].underscore,
+    display_name: network['name'],
+    rpc: rpc_url,
+    explorer: explorer_url,
+    scan_span: 2000
+  )
+end
+puts 'Imported.'
+
+puts '=== Add Pug::EvmContract records ==='
+chain_ids = [421_614, 44]
+chain_ids.each do |chain_id|
+  puts "= Chain ID: #{chain_id}"
+  puts `rails 'pug:add_contract[#{chain_id},0x009D223Aad560e72282db9c0438Ef1ef2bf7703D]'`
+  puts `rails 'pug:add_contract[#{chain_id},0x00BD655DDfA7aFeF4BB109FE1F938724527B49D8]'`
+  puts `rails 'pug:add_contract[#{chain_id},0x003605167cd4C36063a7B63e604497e623Bb8B10]'`
+  puts `rails 'pug:add_contract[#{chain_id},0x00d917EC19A6b8837ADFcF8adE3D6faF62e0F587]'`
+end
+
+puts 'Added.'
