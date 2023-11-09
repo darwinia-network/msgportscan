da508dc2d1ca9519635ad7fd17eb799ac9225565
Aki Wu <wuminzhe@gmail.com>
2023-11-09 23:30:54 +0800

    fix

--- db/migrate/20231109150448_add_timestamps_to_messages.rb
+++ db/migrate/20231109150448_add_timestamps_to_messages.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToMessages < ActiveRecord::Migration[7.1]
   def change
-    add_column :messages, :created_at, :datetime, null: false
-    add_column :messages, :updated_at, :datetime, null: false
+    add_column :messages, :created_at, :datetime
+    add_column :messages, :updated_at, :datetime
   end
 end
